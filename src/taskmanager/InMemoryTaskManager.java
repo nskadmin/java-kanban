622e2ce06555e051b6cdb1b29b4b80051bbe3841
nskadmin <140231861+nskadmin@users.noreply.github.com>
2024-02-27 10:28:07 +0700

    Update InMemoryTaskManager.java

--- src/taskmanager/InMemoryTaskManager.java
+++ src/taskmanager/InMemoryTaskManager.java
@@ -16,10 +16,7 @@ public class InMemoryTaskManager implements TaskManager {
     protected final Map<Integer, Epic> epicCollection = new HashMap<>();
     protected int nextId = 0;
 
-    public HistoryManager getHistoryManager() {
-        
-        return historyManager;
-    }
+    public HistoryManager getHistoryManager() {return historyManager;}
 
     private final HistoryManager historyManager = Managers.getDefaultHistory();
 
@@ -34,11 +31,8 @@ public class InMemoryTaskManager implements TaskManager {
 
         for (Integer epicSubTask : epic.getEpicSubTasks()) {
             
-            if (subtaskCollection.get(epicSubTask).getStatus().equals(TaskStatus.NEW)) {
-                countNew++;
-            } else if (subtaskCollection.get(epicSubTask).getStatus().equals(TaskStatus.DONE)) {
-                countDone++;
-            }
+            if (subtaskCollection.get(epicSubTask).getStatus().equals(TaskStatus.NEW)) countNew++;
+            else if (subtaskCollection.get(epicSubTask).getStatus().equals(TaskStatus.DONE)) countDone++;
         }
         if (countDone == epicSize) {
             epic.setStatus(TaskStatus.DONE);
